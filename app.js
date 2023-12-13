14b13d1b07a9c9d7b81e2e7101b1580c13764a0b
nicholasjamessmith <njsmith6@gmail.com>
2023-12-13 13:19:55 -0500

    Todo app working, added responsive styles

--- app.js
+++ app.js
@@ -14,9 +14,33 @@ const Todo = {
   //Retrieve our todos
   getTodos: () => { return Todo.todos },
   //Adds a new todo
-  createTodo: () => { Todo.todos.push(newTodo) },
+  createTodo: (newTodo) => {
+    Todo.todos.push(newTodo)
+    Todo.saveTodos()
+  },
   //Remove todo
-  deleteTodo: (index) => { Todo.todos.splice(index, 1) }
+  deleteTodo: (index) => {
+    Todo.todos.splice(index, 1)
+    Todo.saveTodos()
+  },
+  // Save Todos to localstorage
+  
+  saveTodos: () => {
+    const arrayString = JSON.stringify(Todo.todos)
+    // save array string in localstorage
+    localStorage.setItem("todos", arrayString)
+  },
+  //Load Todos from Localstorage
+  loadTodos: () => {
+    // get the string from localStorage
+    const arrayString = localStorage.getItem("todos")
+    if (arrayString) {
+      // turn the string back into an array
+      const todos = JSON.parse(arrayString)
+      //update the list of todos
+      Todo.todos = todos
+    }
+  }
 }
 
 //Render function
@@ -35,10 +59,38 @@ const render = () => {
     //add a class of "todo" to the div
     div.classList.add("todo")
     
-    
     //Append the div to the main
     main.append(div)
+    
+    div.addEventListener("click", () => {
+      // delete the todo
+      Todo.deleteTodo(index)
+      //update the DOM
+      render()
+    })
   })
 }
-
+Todo.loadTodos()
 render()
+
+// Handle the form submission
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event) => {
+  //Prevents page refresh when form submission button clicked
+  event.preventDefault()
+  
+  //generate a formData object
+  const formData = new FormData(form)
+  
+  //console.log(formData.get("todo"))
+  
+  //create a new todo object
+  const newTodo = { text: formData.get("todo") }
+  
+  //add the todo to array
+  Todo.createTodo(newTodo)
+  
+  //update the DOM
+  render()
+})
